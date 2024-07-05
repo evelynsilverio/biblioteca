9a4812d047b39fc574ee1880bd4500e6c5ced3ae
evelynsilverio <sebastiansilverioevelyn@gmail.com>
2024-07-05 10:09:12 -0600

    Commit inicial

---
+++ main.py
@@ -0,0 +1,15 @@
+import psycopg2
+from flask import Flask, redirect, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
