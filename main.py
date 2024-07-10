8ba485cbe170b7d651f2a41604eed866264333e0
evelynsilverio <sebastiansilverioevelyn@gmail.com>
2024-07-10 12:58:55 -0600

    Autores

--- main.py
+++ main.py
@@ -25,13 +25,35 @@ def libros():
     #Crear un cursor(objeto para recorrer las tablas)
     cursor = conexion.cursor()
     #Ejecutar una consulta en postgres
-    cursor.execute('''SELECT * FROM libro''')
+    cursor.execute('''SELECT * FROM libros_view''')
     #Recuperar la información
     datos = cursor.fetchall()
     #Cerrar cursos y conexión a la base de datos
     cursor.close()
     conexion.close()
     return render_template('libros.html', datos=datos)
+
+@app.route('/autores')
+def autores():
+
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="chikis03",
+        host="localhost",
+        port="5432"
+    )
+
+    #Crear un cursor(objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    #Ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM autores_view''')
+    #Recuperar la información
+    datos = cursor.fetchall()
+    #Cerrar cursos y conexión a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('autores.html', datos=datos)
 
 if __name__ == '__main__':
     app.run(debug=True)
