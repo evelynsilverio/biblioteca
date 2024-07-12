fd547a3f5b8fe383eb5db448e3bdff38007408d4
evelynsilverio <sebastiansilverioevelyn@gmail.com>
2024-07-12 09:29:01 -0600

    Paises.html

--- main.py
+++ main.py
@@ -55,6 +55,29 @@ def autores():
     conexion.close()
     return render_template('autores.html', datos=datos)
 
+@app.route('/paises')
+def paises():
+    #Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="chikis03",
+        host="localhost",
+        port="5432"
+    )
+
+    #Crear un cursor(objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    #Ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM pais''')
+    #Recuperar la información
+    datos = cursor.fetchall()
+    #Cerrar cursos y conexión a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('paises.html', datos=datos)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
